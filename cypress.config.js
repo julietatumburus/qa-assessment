1188936121694b5d6c7abd57d8645bb735e07f64
juli <julitumburus@gmail.com>
2026-02-07 14:07:56 -0300

    Add pageloadtimeout

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     baseUrl: 'https://www.saucedemo.com',
     viewportWidth: 1280,
     viewportHeight: 720,
+    pageLoadTimeout: 120000,
     video: true,
     screenshotOnRunFailure: true,
   },
